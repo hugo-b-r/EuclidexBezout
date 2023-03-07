1eeb0aa49186193e5d0de7002d578752d12635b5
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-03-07 20:17:17 +0100

    feat: impl new pour AlgoEuclide permet de faire passer le premier test

--- src/lib.rs
+++ src/lib.rs
@@ -15,16 +15,28 @@ struct AlgoEuclide {
     lignes: Vec<Brique>, 
 }
 
+impl AlgoEuclide {
+    fn new(a: i32, b:i32) -> Self {
+        AlgoEuclide {
+            a,
+            b,
+            lignes: Vec::new(),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::AlgoEuclide;
     #[test]
     fn test_algo_euclide() { //teste la creation algorithem d'euclide
-        let algorithme = AlgoEuclide::new();
-        if algorithme.a == 0 && algorithme.b == 0 && algorithme.lignes == Vec::default() {
-            let result = true;
+        let algorithme = AlgoEuclide::new(0, 0);
+        let mut result = bool::default();
+        if algorithme.a == 0 && algorithme.b == 0 {
+            result = true;
         } else {
-            let result = false;
+            result = false;
         }
-        assert!(result, true);
+        assert_eq!(result, true);
     }
 }
