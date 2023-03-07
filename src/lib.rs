229bfb6bde06f9e18dcccca2e84a64fd08c68cd6
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-03-07 20:11:00 +0100

    feat: test AlgoEuclide
    ecriture d'un test qui verifie la bonne creation d'un algorithme d'euclide
    a l'aide du constructeur new

--- src/lib.rs
+++ src/lib.rs
@@ -14,3 +14,17 @@ struct AlgoEuclide {
     b: i32,
     lignes: Vec<Brique>, 
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_algo_euclide() { //teste la creation algorithem d'euclide
+        let algorithme = AlgoEuclide::new();
+        if algorithme.a == 0 && algorithme.b == 0 && algorithme.lignes == Vec::default() {
+            let result = true;
+        } else {
+            let result = false;
+        }
+        assert!(result, true);
+    }
+}
