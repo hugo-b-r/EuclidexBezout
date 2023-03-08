6bb146293cc5134c908ae91680b660d306855d1a
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-03-08 20:19:15 +0100

    feat: function to get pgcd

--- src/lib.rs
+++ src/lib.rs
@@ -124,4 +124,10 @@ fn division_euclidienne(mut a: i32, mut b: i32) -> (i32, i32) {
     (quotient, reste)
 }
 
+fn pgcd(a: i32, b: i32) -> i32 {
+    let mut algorithme = AlgoEuclide::new(a, b);
+    algorithme = algorithme.compute();
+    algorithme.lignes[(algorithme.lignes.len()-1)].nombres[0] as i32
+}
+
 
