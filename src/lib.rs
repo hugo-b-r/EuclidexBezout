1df7475bbe551db08a1393457c4fb203d1cd3e95
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-03-23 19:56:58 +0100

    depart de rien pour mieux sauter

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,10 @@
 mod algorithme_euclide;
-mod type_operation;
 mod brique;
-mod generic;
 
 
 
-pub use crate::generic::*;
 pub use crate::algorithme_euclide::*;
 pub use crate::brique::*;
-pub use crate::type_operation::*;
 
 
 
