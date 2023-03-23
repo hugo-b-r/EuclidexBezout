d759f6d9003edf51be7d927da82d734fa75c7281
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-03-23 20:51:12 +0100

    fonction pour obtenir la valeur d'une brique avec un rang, demande mais pas forcement utilise en fonction du type

--- src/brique.rs
+++ src/brique.rs
@@ -10,6 +10,81 @@ enum Brique {
 }
 
 impl Brique {
+    fn valeur(self: &Self, rang: usize) -> Result<i64, String> {
+        match self {
+            Entier(valeur) => valeur,
+            Produit(facteur_1, facteur_2) => {
+                if rang > 1 {
+                    Err(String::from("rang hors du nombre de facteurs"))
+                } else if rang == 0 {
+                    if let Brique::Entier(entier) = facteur_1 {
+                        Ok( facteur_1 )
+                    } else {
+                        Err(String::from("facteur à ce rang n'est pas un nombre entier"))
+                    }
+                } else if rang == 1 {
+                    if let Brique::Entier(entier) = facteur_2 {
+                        Ok( facteur_2 )
+                    } else {
+                        Err(String::from("facteur à ce rang n'est pas un nombre entier"))
+                    }
+                }
+            },
+            Somme(terme_1, terme_2) => {
+                if rang > 1 {
+                    Err(String::from("rang hors du nombre de termes"))
+                } else if rang == 0 {
+                    if let Brique::Entier(entier) = terme_1 {
+                        Ok( terme_1 )
+                    } else {
+                        Err(String::from("terme à ce rang n'est pas un nombre entier"))
+                    }
+                } else if rang == 1 {
+                    if let Brique::Entier(entier) = terme_2 {
+                        Ok( terme_2 )
+                    } else {
+                        Err(String::from("terme à ce rang n'est pas un nombre entier"))
+                    }
+                }
+            },
+            Difference(terme_1, terme_2) => {
+                if rang > 1 {
+                    Err(String::from("rang hors du nombre de termes"))
+                } else if rang == 0 {
+                    if let Brique::Entier(entier) = terme_1 {
+                        Ok( terme_1 )
+                    } else {
+                        Err(String::from("terme à ce rang n'est pas un nombre entier"))
+                    }
+                } else if rang == 1 {
+                    if let Brique::Entier(entier) = terme_2 {
+                        Ok( terme_2 )
+                    } else {
+                        Err(String::from("terme à ce rang n'est pas un nombre entier"))
+                    }
+                }
+            },
+            Division(terme_1, terme_2) => {
+                if rang > 1 {
+                    Err(String::from("rang hors du nombre de facteurs"))
+                } else if rang == 0 {
+                    if let Brique::Entier(entier) = facteur_1 {
+                        Ok( facteur_1 )
+                    } else {
+                        Err(String::from("facteur à ce rang n'est pas un nombre entier"))
+                    }
+                } else if rang == 1 {
+                    if let Brique::Entier(entier) = facteur_2 {
+                        Ok( facteur_2 )
+                    } else {
+                        Err(String::from("facteur à ce rang n'est pas un nombre entier"))
+                    }
+                }
+            },
+            DivisionEuclidienne(Box<Self>, Box<Self>, Box<Self>) => Err(String::from("Valeur impossible pour une division euclidienne")),
+        }
+    }
+
     fn developpe(self: &Self) -> Result<Self, String> {
         if let Brique::Produit(brique_1, brique_2) = self {
             if let Brique::Somme(entier_1, entier_2) = brique_1 {
