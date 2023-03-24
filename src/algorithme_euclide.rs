745da1167e2e6f2544c77a2140d4d14ae35c072c
LeMoustachu <hugoberthetrambaud@outlook.com>
2023-03-24 15:36:18 +0100

    division euclidienne

--- src/algorithme_euclide.rs
+++ src/algorithme_euclide.rs
@@ -5,3 +5,42 @@ struct AlgorithmeEuclide {
     a: i64,
     b: i64,
 }
+
+fn division_euclidienne(a: i64, b: i64) -> Brique {
+    let mut c;
+    let mut d;
+    if a < b {
+        c = b;
+        d = a;
+    } else {
+        c = a;
+        d = b;
+    }
+
+    let quotient = c/d;
+    let reste = c - d*quotient;
+    Brique::Egalite(
+        vec![
+            Box::new(
+                Brique::Entier(c)
+            ),
+            Box::new(
+                Brique::Somme(
+                    vec![
+                        Box::new(
+                            Brique::Produit(
+                                vec![
+                                    Box::new(Brique::Entier(d)),
+                                    Box::new(Brique::Entier(quotient)),
+                                ]
+                            )
+                        ),
+                        Box::new(
+                            Brique::Entier(reste)
+                        )
+                    ]
+                )
+            )
+        ]
+    )
+}
