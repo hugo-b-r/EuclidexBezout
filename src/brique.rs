e38739fe4cd4a4a49b1de402c709430585b5d718
Hugo Berthet-Rambaud <hugoberthetrambaud@outlook.com>
2023-03-25 08:03:56 +0100

    mise a jour de l'en-tete des fichiers sources

--- src/brique.rs
+++ src/brique.rs
@@ -1,7 +1,9 @@
 /*
     2023 Hugo Berthet-Rambaud
     Licencié sous la licence MIT. La districution de ce code sans cette licence
-    ne peut se faire.
+    n'est pas prévue.
+        Cette licence est trouvable à l'addresse:
+    https://github.com/hugo-b-r/EuclidexBezout/blob/master/LICENSE.md
 
     --- Pour l'humanité, son bonheur et sa gloire ---
  */
