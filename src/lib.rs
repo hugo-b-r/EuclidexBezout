c8b20c15608e20bb1ffc39cba8a8e19f02f3a037
Hugo Berthet-Rambaud <hugoberthetrambaud@outlook.com>
2023-03-25 08:05:14 +0100

    mise a jour du lien vers la licence dans l'en-tete

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
     Licencié sous la licence MIT. La districution de ce code sans cette licence
     n'est pas prévue.
         Cette licence est trouvable à l'addresse:
-    https://github.com/hugo-b-r/EuclidexBezout/blob/master/LICENSE.md
+    https://raw.githubusercontent.com/hugo-b-r/EuclidexBezout/master/LICENSE.md
 
     --- Pour l'humanité, son bonheur et sa gloire ---
  */
